7502190c0535c43f4ff809de90dd506b909032cd
chenweimin <chenweimin@workmagic.io>
2023-12-26 12:20:05 +0800

    fix: 修改reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   // forbidOnly: !!process.env.CI,
   // retries: process.env.CI ? 2 : 0,
   workers:  process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: 'github',
   // reporter: [
   //   ['list', { printSteps: true }]
   //   ['line'],
